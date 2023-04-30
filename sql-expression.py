69955aec1efc58bdf925e7ca879b650de5ce25be
Tonywilson1211 <>
2023-04-30 17:50:52 +0000

    bug fix

--- sql-expression.py
+++ sql-expression.py
@@ -59,6 +59,6 @@ with db.connect() as connection:
 # Query 6 - select all tracks where the composer is 'Queen' from the "Track"
 # select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
-results = connection.execute(select_query)
-for result in results:
-    print(result)
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
